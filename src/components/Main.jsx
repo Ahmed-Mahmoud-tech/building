f0e96acff495eea67d74f2945b87d9be8ef02e9d
Ahmed Mahmoud <ahmedmahmoudtech@gmail.com>
2023-12-24 02:05:43 +0200

    mobile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,15 @@
 import React, { useEffect, useState } from 'react';
 import { Joystick } from 'react-joystick-component';
 import Script from 'next/script'
+import { IoFootstepsSharp } from "react-icons/io5";
+import { Wrapper } from './Main.styled';
+import feet from '../assets/images/feet.jpg';
+import Image from 'next/image'
+
 
 let rotationInterval;
+let walkingInterval;
+let WalkingStatus = "stop"
 let x;
 let y;
 
@@ -22,6 +29,7 @@ const Main = () => {
   
   
 const [moveType, setMoveType] = useState("stop")
+const [sliderValue, setSliderValue] = useState(50)
 const [animation, setAnimation] = useState("Static Pose")
 
   useEffect(() => {
@@ -29,13 +37,64 @@ const [animation, setAnimation] = useState("Static Pose")
     setTimeout(() => {
       setAnimation("Take 001")
     }, 15000);
+ 
+
   }, [loaded])
+
+
+    function moveCameraForward() {
+      // Get the camera entity
+       const camera =  document.querySelector('a-camera');
+      // Get the current position and rotation of the camera
+      const currentPosition = camera.getAttribute('position');
+      const currentRotation = camera.getAttribute('rotation');
+
+      // Convert rotation to radians
+      const radians = currentRotation.y * (Math.PI / 180);
+
+      // Calculate the new position based on rotation
+      const newPosition = {
+        x: currentPosition.x - Math.sin(radians) / 50 * sliderValue,  // Adjust based on the desired distance
+        y: currentPosition.y,
+        z: currentPosition.z - Math.cos(radians) / 50 * sliderValue,  // Adjust based on the desired distance
+      };
+
+      // Update the camera position
+      // camera.setAttribute('position', newPosition);
+
+        camera.setAttribute('animation', {
+        property: 'position',
+        to: `${newPosition.x} ${newPosition.y} ${newPosition.z}`,
+        dur: 100,  // Duration in milliseconds
+        easing: 'linear',  // Easing function
+        });
+      
+  }
+  
+  const walking = () => {
+
+    if (WalkingStatus == "stop") {
+      WalkingStatus = "start";
+   
+    walkingInterval = setInterval(() => { 
+
+moveCameraForward()
+
+    }, 100)
+
+    } else {
+       WalkingStatus = "stop";
+      clearInterval(walkingInterval)
+       }
+  }
+
 
   const camRotation = () => {
 
-      document.querySelector('a-camera').setAttribute("look-controls" , 'enabled:true');
-
     let aCamEl = document.querySelector('a-camera');
+
+      aCamEl.setAttribute("look-controls" , 'enabled:true');
+
       rotationInterval = setInterval(changeRotation, 50);
       function changeRotation () {
         camRotationX += x ? x/50 : 0 
@@ -43,38 +102,52 @@ const [animation, setAnimation] = useState("Static Pose")
         
        aCamEl.components['look-controls'].pitchObject.rotation.set(camRotationY, 0, 0);
       aCamEl.components['look-controls'].yawObject.rotation.set(0, -camRotationX, 0);
- 
-        
     };
  
   }
+
+  const toggleWalk = () => {
+
+    walking();
+    setTimeout(() => {
+      walking()
+    }, 100);
+
+  }
+    
   
   return (
     <>
       
-                 <Script src={'/assets/aframe.min.js'} strategy="beforeInteractive" />
+      <Script src={'/assets/aframe.min.js'} strategy="beforeInteractive" />
       <Script src={'/assets/aframe-extras.js'} strategy="beforeInteractive" />
       
-      {loaded && <>
-        <div style={{ zIndex: "10000", position: "absolute" }}>
-        
-          <Joystick size={100} sticky={false} baseColor="red" stickColor="blue" start={(e) => { setMoveType("start"), camRotation() }} move={(e) => { setX(e.x); setY(e.y) }} stop={() => { setMoveType("stop"); clearInterval(rotationInterval); document.querySelector('a-camera').setAttribute("look-controls", 'enabled:false') }
-          }></Joystick>
+      {loaded && <Wrapper feetColor={"orange"}>
+
+        <div className="sliderContainer z-20 absolute flex left-[50%] top-4 translate-x-[-50%] items-center w-1/2">
+            <Image src={feet} alt="" className='w-6' />
+          <input type="range" min="0" max="100" value={sliderValue} id="myRange" className='slider m-2 slider' onChange={(e) => { setSliderValue(e.target.value); toggleWalk(); console.log(e.target.value)}}  />
+            <Image src={feet} alt=""  className='w-10'/>
         </div>
+
+        <div className=' absolute z-20 left-0 bottom-0  m-10 '>
+          <Joystick size={75} sticky={false} baseColor="black" stickColor="orange" start={(e) => { setMoveType("start"), camRotation() }} move={(e) => { setX(e.x); setY(e.y) }} stop={() => { setMoveType("stop"); clearInterval(rotationInterval); document.querySelector('a-camera').setAttribute("look-controls", 'enabled:false') } }></Joystick>
+        </div>
+        
+        <button className=' absolute bottom-0 right-0 p-4 z-20  text-5xl m-10 feet' onClick={walking}> <IoFootstepsSharp /> </button>
 
         <a-scene cursor="rayOrigin: mouse">
   
-          <a-camera id="camera" position="0 2 0" look-controls="enabled:false" ></a-camera>
+          <a-camera id="camera" position="0 2 0" look-controls="enabled:true" ></a-camera>
 
           <a-box cursor-listener position="0 1 0" rotation="0 0 0" color="gray" width="1" height="1" onClick={() => alert("Camera")}
             depth="40" side="double" scale="1 1 1" id="room"
             material="roughness: 1; metalness: 1;shader: standard; emissiveIntensity:1; dithering:true; emissive:black" />
 
           <a-entity shadow="receive: false; cast:true" gltf-model="url(/assets/3dModel/wallace_wells_and_scott_pilgrims_apartment/scene.gltf)" cursor-listener rotation="0 0 0" scale="5 5 5"  ></a-entity>
-        
-
+      
         </a-scene>
-      </>
+      </Wrapper>
       }
      </>
 
