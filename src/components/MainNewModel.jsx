1c4264d4fa927c49631104b40ce2739518e53c44
Ahmed Mahmoud <ahmedmahmoudtech@gmail.com>
2023-12-31 00:58:52 +0200

    step one

--- src/components/MainNewModel.jsx
+++ src/components/MainNewModel.jsx
@@ -181,7 +181,7 @@ function isPointInPolygon(vertices, point) {
       // Update the camera position
       // camera.setAttribute('position', newPosition);
 
-    if (isPointInPolygon(areaCoordinates,  { x: newPosition.x, y: newPosition.z })) {
+    // if (isPointInPolygon(areaCoordinates,  { x: newPosition.x, y: newPosition.z })) {
       
       camera.setAttribute('animation', {
       property: 'position',
@@ -189,7 +189,7 @@ function isPointInPolygon(vertices, point) {
       dur: 100,  // Duration in milliseconds
       easing: 'linear',  // Easing function
       });
-    }
+    // }
   }
 
 
@@ -259,10 +259,11 @@ function isPointInPolygon(vertices, point) {
         <a-scene cursor="rayOrigin: mouse">
   
           {/* <a-camera id="camera" position="0 2 0" look-controls="enabled:true" ></a-camera> */}
-          <a-camera id="camera" position="0 2 0" look-controls="enabled:true" wasd-controls ></a-camera>
+          <a-camera id="camera" position="0 9 0" look-controls="enabled:true" wasd-controls ></a-camera>
           {/* <a-box cursor-listener="true" position="-11.4503 6.47691 -5.7" rotation=""  side="double" opacity="0" scale="" id="room" material="roughness: 1; metalness: 1; emissive: transparent" geometry="width: 0.73; height: 6.39; depth: 4.22" onClick={() => setPopStatus(true)} ></a-box> */}
           <a-box   position="-11.4503 6.47691 -5.7"   side="double"    material="  color:red"  geometry="width: 1; height: 1; depth: 1"></a-box>
-          <a-entity shadow="receive: false; cast:true" gltf-model="url(/assets/3dModel/wallace_wells_and_scott_pilgrims_apartment/scene.gltf)" cursor-listener rotation="0 0 0" scale="5 5 5"  ></a-entity>
+          <a-entity shadow="receive: false; cast:true" gltf-model="url(/assets/3dModel/the_big_bang_theory_virtual_apartment/scene.gltf)" cursor-listener rotation="0 0 0" scale="5 5 5" position="-80 0 50" ></a-entity>
+          {/* <a-entity shadow="receive: false; cast:true" gltf-model="url(/assets/3dModel/the_big_bang_theory_virtual_apartment/scene.gltf)" cursor-listener rotation="0 0 0" scale="5 5 5" position="-80 0 50" ></a-entity> */}
       
         </a-scene>
 
